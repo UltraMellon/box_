20e66be44c52028cb5b293dee427f0ac333fe4c2
UltraMellon <121882633+UltraMellon@users.noreply.github.com>
2023-12-28 10:29:08 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200, 200, 100, 100);
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+  if(keyIsDown(UP_ARROW)){
+    box.y -= 5
+  }
+  elsenif(keyIsDown(DOWN_ARROW)){
+    box.y += 5
+}
+
+
+
+
